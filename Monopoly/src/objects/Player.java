471a1cfcb91b6e2e8068abaa5c42bac3f1864e60
wsmitche <wsmitche@ncsu.edu>
2018-09-12 00:05:27 -0700

    getting thoughts together

--- Monopoly/src/objects/Player.java
+++ Monopoly/src/objects/Player.java
@@ -1,5 +1,13 @@
 package objects;
 
 public class Player {
+	/** Starting cash **/
+	static final int STARTING_CASH = 1500;
+	
+	/** player Cash **/
+	private int cash;
+	
+	/** player properties **/
+	private ArrayList<Properties> property;
 
 }
--- Monopoly/src/objects/Tile.java
+++ Monopoly/src/objects/Tile.java
@@ -1,5 +1,16 @@
 package objects;
 
 public class Tile {
+//Types of tiles:
+	//Go
+	//jail
+	//free parking
+	//go to jail
+	//Lux Tax
+	//prop tax
+	//property
+		//railroads
+		//utilities
 
+	
 }
